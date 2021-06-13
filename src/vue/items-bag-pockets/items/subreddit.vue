844c319b41b5a6603eed5950a1911e7133704c9e
Artūras Šelechov-Balčiūnas <shelekhov.arthur@gmail.com>
2021-06-13 15:52:04 +0200

    Render subreddits

---
+++ src/vue/items-bag-pockets/items/subreddit.vue
@@ -0,0 +1,58 @@
+<template>
+  <div :class="{box: true, 'is-ticked': isTicked}">
+    <template v-if="value.kind === 't5'">
+      <div class="subreddit">
+        <input type="checkbox" v-model="isTicked" />
+        <img :src="value.image" alt="Subreddit's logo"/>
+        <article>
+          <header>{{value.data.title}}</header>
+          <p>{{value.data.public_description}}</p>
+        </article>
+      </div>
+    </template>
+
+    <template v-else>
+      <p class="invalid-item-type">Not a subreddit.</p>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'subreddit',
+  props: {
+    value: Object,
+  },
+  data() {
+    return {
+      isTicked: false,
+    };
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.subreddit {
+  position: relative;
+  display: flex;
+
+  > input[type="checkbox"] {
+    position: absolute;
+    top: 5px;
+    left: 5px;
+  }
+
+  > img {
+    width: 60px;
+    display: block;
+    margin-right: 10px;
+  }
+
+  > article {
+    > header {
+      font-weight: bold;
+      margin-bottom: 10px;
+    }
+  }
+}
+</style>
--- src/vue/items-bag-pockets/subreddits-subscribed.vue
+++ src/vue/items-bag-pockets/subreddits-subscribed.vue
@@ -1,23 +1,54 @@
 <template>
   <div>
     <header>Subscriptions</header>
-    <p>Here goes the list of subreddits you're subscribed to.</p>
-    <p>Token is: {{token}}</p>
+
+    <p v-if="subreddits === null">Loading...</p>
+    <div v-else-if="typeof subreddits === typeof undefined">
+      <p>Error on loading.</p>
+      <button type="button" @click.prevent="reload">Reload</button>
+    </div>
+    <template v-else>
+      <subreddit v-for="subreddit in subreddits" :key="subreddit.data.id" :value="subreddit"></subreddit>
+    </template>
   </div>
 </template>
 
 <script>
 import RedditService from '../../reddit-service';
+import Subreddit from './items/subreddit';
 
 export default {
   name: 'subreddits-subscribed',
+  components: {Subreddit},
   props: {
     token: String,
   },
 
-  mounted() {
-    RedditService.subreddits.get(this.token);
+  data() {
+    return {
+      subreddits: null,
+    }
   },
+
+  async mounted() {
+    await this.reload();
+  },
+
+  methods: {
+    async reload() {
+      this.subreddits = null;
+      try {
+        const response = await RedditService.subreddits.get(this.token);
+        if (response.data) {
+          this.subreddits = response.data;
+        } else {
+          this.subreddits = undefined;
+        }
+      } catch (e) {
+        this.subreddits = undefined;
+      }
+    },
+  }
 }
 </script>
 
