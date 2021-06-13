8de57689d3fae5c4ff054d201bd6367a90fea07d
Artūras Šelechov-Balčiūnas <shelekhov.arthur@gmail.com>
2021-06-13 15:51:35 +0200

    Enable SCSS in VueJS

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,16 @@ module.exports = {
                     'css-loader'
                 ]
             },
+            // this will apply to both plain `.scss` files
+            // AND `<style lang="scss">` blocks in `.vue` files
+            {
+                test: /\.scss$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
         ],
     },
     plugins: [
